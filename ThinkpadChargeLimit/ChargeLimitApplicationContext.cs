32a8a4688ce43f7efdcddf628ac110e8b5119899
danielzhang130 <danielzhang130@gmail.com>
2020-12-17 20:24:17 -0500

    Add project files.

---
+++ ThinkpadChargeLimit/ChargeLimitApplicationContext.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ThinkpadChargeLimit
+{
+    internal class ChargeLimitApplicationContext : ApplicationContext
+    {
+        private static readonly string CHARGING_TO_FULL = "Charging to Full";
+        private static readonly string CHARGING_TO_PERCENT = "Charging to {0}%";
+
+        private readonly NotifyIcon notifyIcon;
+        private readonly ChargeThresholdWrapper wrapper;
+
+        private int chargeLimit;
+        private MenuItem prevChecked;
+
+        public ChargeLimitApplicationContext()
+        {
+            wrapper = new ChargeThresholdWrapper();
+
+            chargeLimit = wrapper.Limit;
+
+            MenuItem exitMenuItem = new MenuItem("Exit", new EventHandler(Exit));
+
+            MenuItem[] limitItems = new MenuItem[6];
+            int j = 0;
+            for (int i = 50; i <= 100; i += 10)
+            {
+                limitItems[j] = new MenuItem(i.ToString() + '%', new EventHandler(SetLimit))
+                {
+                    RadioCheck = true
+                };
+
+                if (i == chargeLimit)
+                {
+                    limitItems[j].Checked = true;
+                    prevChecked = limitItems[j];
+                }
+                j++;
+            }
+            MenuItem limitsMenuItem = new MenuItem("Change Charge Limit", limitItems);
+
+            notifyIcon = new NotifyIcon
+            {
+                ContextMenu = new ContextMenu(new MenuItem[]
+                    { limitsMenuItem, exitMenuItem }),
+                Visible = true
+            };
+            notifyIcon.ContextMenu.Popup += new EventHandler(OnShowMenu);
+
+            UpdateState();
+        }
+
+        private void SetLimit(object sender, EventArgs e)
+        {
+            if (sender is MenuItem menuItem)
+            {
+                chargeLimit = int.Parse(menuItem.Text.Substring(0, menuItem.Text.IndexOf('%')));
+                menuItem.Checked = true;
+                // todo needed?
+                prevChecked.Checked = false;
+                prevChecked = menuItem;
+
+                wrapper.Limit = chargeLimit;
+                UpdateState();
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private void OnShowMenu(object sender, EventArgs e)
+        {
+            UpdateState();
+        }
+
+        private void UpdateState()
+        {
+            if (wrapper.HasLimit)
+            {
+                notifyIcon.Icon = Properties.Resources.HalfCharged;
+                notifyIcon.Text = string.Format(CHARGING_TO_PERCENT, chargeLimit);
+            }
+            else
+            {
+                notifyIcon.Icon = Properties.Resources.FullyCharged;
+                notifyIcon.Text = CHARGING_TO_FULL;
+            }
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            // We must manually tidy up and remove the icon before we exit.
+            // Otherwise it will be left behind until the user mouses over.
+            notifyIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
---
+++ ThinkpadChargeLimit/ChargeThresholdWrapper.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace ThinkpadChargeLimit
+{
+    internal class ChargeThresholdWrapper
+    {
+        private static readonly string EXE_PATH = "ChargeThreshold.exe";
+        private static readonly string STOP_AT = "Stop at ";
+
+        internal bool HasLimit
+        {
+            get
+            {
+                return Limit != 100;
+            }
+        }
+
+        internal int Limit
+        {
+            get
+            {
+                int _limit = 100;
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo.FileName = EXE_PATH;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.Arguments = "status";
+                    process.StartInfo.CreateNoWindow = true;
+                    process.Start();
+
+                    StreamReader reader = process.StandardOutput;
+                    string output = reader.ReadToEnd();
+
+                    int index = output.IndexOf(STOP_AT);
+                    if (index != -1)
+                    {
+                        int index_percent = output.IndexOf('%', index + STOP_AT.Length);
+                        string percentage = output.Substring(index + STOP_AT.Length, index_percent - index - STOP_AT.Length);
+                        _limit = int.Parse(percentage);
+                    }
+
+                    process.WaitForExit();
+                }
+
+                return _limit;
+            }
+
+            set
+            {
+                using (Process process = new Process())
+                {
+                    process.StartInfo.FileName = EXE_PATH;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.Arguments = value == 100 ? "off" :  ("on " + value);
+                    process.StartInfo.CreateNoWindow = true;
+                    process.Start();
+                    process.WaitForExit();
+                }
+            }
+        }
+    }
+}
